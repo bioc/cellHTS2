048eff161e8598e2c23694b01ed49900b2c0d619
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2010-01-17 02:15:11 +0000

    A lot of cleaning up: - spatial normalisation (was broken, should be converging to something   better now) - configuration plots (using the better support from the new 'splots')
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cellHTS2@44037 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plateConfModule.R
+++ R/plateConfModule.R
@@ -64,22 +64,22 @@ configurationAsScreenPlot <- function(x, verbose=interactive(), posControls, neg
     ## Check the status of the 'cellHTS' object
     if(!state(x)[["configured"]])
         stop("Please configure 'x' (using the function 'configure') before normalization.")
-    require("splots") || stop("Package 'splots' was not found and needs to be installed.")
+
     wellAnnotation <- as.character(wellAnno(x))
     wellCols <- c(sample="#999999", other="#000000", empty="#FFFFFF")
     pcolPal <- rev(brewer.pal(9, "Reds")[-c(1,2,9)])
     ncolPal <- rev(brewer.pal(9, "Blues")[-c(1,2,9)])
     if(missing(negControls)) negControls <- "^neg$"
     if(missing(posControls)) posControls <- "^pos$"
-    ##this gives the index in wellAnno(x)
+    ## this gives the index in wellAnno(x)
     negInd <- findControls(negControls, as.character(wellAnno(x)))  
     posInd <- findControls(posControls, as.character(wellAnno(x)))
     aux <- if(is.list(posInd)) sapply(posInd, length)==0 else length(posInd)==0
-    if(any(aux) & verbose)
+    if(any(aux) && verbose)
         warning(sprintf("'%s' not found among the well annotation!\n",
                         posControls[which(aux)]))
     aux <- if(is.list(negInd)) sapply(negInd, length)==0 else length(negInd)==0
-    if(any(aux) & verbose)
+    if(any(aux) && verbose)
         warning(sprintf("'%s' not found among the well annotation!\n",
                         negControls[which(aux)]))
     namePos <- unique(sapply(posInd, function(i) unique(wellAnnotation[i])))
@@ -103,26 +103,25 @@ configurationAsScreenPlot <- function(x, verbose=interactive(), posControls, neg
     ## remove unused well annotation
     wellCols <- wellCols[names(wellCols) %in% unique(wellAnnotation)]
     mtW <- match(wellAnnotation, names(wellCols))
-    if(any(is.na(mtW)))
-    {
-        wh <- is.na(mtW)
+    wh <- is.na(mtW)
+    if(length(wh)>0)  {
         notCovered <- unique(wellAnnotation[wh])
         notCov <- substr(rainbow(length(notCovered)), 1, 7)
         names(notCov) <- notCovered
         wellCols <- c(wellCols, notCov)
         mtW[wh] <- match(wellAnnotation[wh], names(wellCols))
-    }
-    ## simple configuration based exclusively on plate configuration file (i.e. without any
-    ## update for flagged wells over plates, replicates or channels)
-    ## just a single image plot of all plates:
-    ldat <- split(mtW, plate(x))
-    grid.newpage()
-    fill <- if(length(wellCols)>1) wellCols else rep(wellCols, 2)
-    zrange <- if(length(wellCols)>1) range(mtW) else c(0, 1)
-    plotScreen(ldat, zrange=zrange, fill=fill, do.legend=legend,
-               legend.mapping=names(wellCols), main=main,
-               nx=pdim(x)[["ncol"]], ny=pdim(x)[["nrow"]],
-               ncol=ifelse(max(plate(x)) < 6L, max(plate(x)), 6L))
+      }
+    mtW = factor(mtW)
+    levels(mtW) = names(wellCols)
+    
+    plotScreen(split(mtW, plate(x)),
+               fill = wellCols,
+               do.legend = legend,
+               main = main,
+               nx = pdim(x)[["ncol"]],
+               ny = pdim(x)[["nrow"]],
+               ncol = ifelse(max(plate(x)) < 6L, max(plate(x)), 6L))
+    
     ## put correct names (name given in the conf file)
     aux <- sapply(names(wellCols), function(i)
                   plateConf(x)$Content[match(i, tolower(plateConf(x)$Content))]) 
