73946920be4abc533ad680e6bfb6abc1423b3edd
Wolfgang Huber <wolfgang.huber@embl.org>
2024-03-18 11:19:54 +0100

    Improved deprecation msg

--- R/zzz.R
+++ R/zzz.R
@@ -4,9 +4,9 @@
 
 .onAttach <- function(libname, pkgname) {
     msg <- sprintf(
-        "Package '%s' is deprecated and will be removed from Bioconductor
-         version %s", pkgname, "3.20")
-    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+        "Package '%s' is deprecated and will be removed from Bioconductor version %s.", pkgname, "3.20") |>
+      paste("We recommend using tidy data structures, dplyr and its aggretation functions, and ggplot2 instead.")
+    .Deprecated(msg = paste(strwrap(msg, exdent=2), collapse="\n"))
    ## set up menus -- windows only for now
    if( .Platform$OS.type == "windows" && .Platform$GUI == "Rgui" )
       addVigs2WinMenu("cellHTS2") ## in Biobase
